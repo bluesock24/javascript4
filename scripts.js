24a4ec8d0953385ed614fc0f1f300a84b5aba36a
tajson <laski.tajka@gmail.com>
2017-03-30 10:19:11 +0200

    make js equation

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var a = 4;
+var b = 5;
+var value = (a * a) + (2 * a * b) - (b * b);
+console.log('Wynik to: ' + value)
+if (value < 0) {
+	console.log('Wynik jest ujemny')
+}
+else if (value = 0) {
+	console.log('Wynik jest równy 0')
+}
+else {
+	console.log('Wynik jest dodatni')
+}
